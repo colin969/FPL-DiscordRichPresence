5260aa875ff4069f8994154b35207700da0e54ef
Colin Berry <colinb969@gmail.com>
2020-11-12 15:58:05 +0000

    Initial Release 9.1.0

---
+++ README.md
@@ -0,0 +1,22 @@
+# Discord Rich Presence for Flashpoint Launcher
+
+A Flashpoint Launcher extension that provides support for Discord Rich Presence. (https://discord.com/rich-presence)
+
+## Usage
+
+Make sure Game Activity is enabled in your Discord settings and that you're using the App, not the Browser version.
+
+When you play a game or animation on Flashpoint your Discord presence will be updated to display it.
+
+Extreme games will be hidden by default, see `Display Extreme Games` in Config to enable it.
+
+## Building
+
+1) Clone into `\Data\Extensions` in the Flashpoint folder.
+
+2) `npm install`
+
+3) `npm run build` (Use `npm run package` for production versions)
+
+4) Start Flashpoint Launcher
+
---
+++ src/ext.ts
@@ -0,0 +1,87 @@
+import * as flashpoint from 'flashpoint-launcher';
+import * as DiscordRPC from 'discord-rpc';
+import { debounce } from 'ts-debounce';
+
+const clientId = "732942533373460560"; 
+
+export async function activate(context: flashpoint.ExtensionContext) {
+  const registerSub = (d: flashpoint.Disposable) => { flashpoint.registerDisposable(context.subscriptions, d)};
+  let curActivity = createActivity();
+  let curGame: flashpoint.Game | undefined = undefined;
+
+  const client = new DiscordRPC.Client({ transport: 'ipc' });
+  client.on('ready', () => {
+    flashpoint.log.debug('Discord RPC Connected!');
+    setActivity(client, curActivity);
+    setInterval(() => {
+      setActivity(client, curActivity);
+    }, 15000);
+  });
+  client.login({ clientId }).catch(flashpoint.log.error);
+
+  flashpoint.games.onDidLaunchGame((game) => {
+    if (!flashpoint.getExtConfigValue('com.discord-rich-presence.show-extreme') && game.extreme) { return; }
+    curActivity = createActivity(game);
+    curGame = game;
+  });
+
+  flashpoint.services.onServiceRemove((process) => {
+    if (process.id.startsWith('game.') && process.id.length > 5) {
+      let closedId = process.id.substr(5);
+      if (closedId === curGame.id) {
+        curActivity = createActivity();
+        curGame = undefined;
+      }
+    }
+  })
+
+  registerSub(flashpoint.newDisposable(() => {
+    flashpoint.log.debug('Shutting down Discord RPC client...');
+    client.destroy();
+  }))
+}
+
+async function setActivity(client: DiscordRPC.Client, activity: DiscordRPC.Presence) {
+  return client.setActivity(activity);
+};
+
+function createActivity(game?: flashpoint.Game): DiscordRPC.Presence {
+  if (game) {
+    flashpoint.log.debug(`Game:\n ${JSON.stringify(game, null, 2)}`);
+    const playing = game.library === 'arcade';
+    const imageKey = playing ? 'game' : 'animation';
+    const imageText = playing ? 'Game' : 'Animation';
+    const owner = game.developer !== '' ? game.developer :
+      game.publisher !== '' ? game.publisher : undefined;
+    const state = owner ? formatOwners(owner) : game.platform;
+    return { 
+      details: game.title,
+      state: state,
+      startTimestamp: new Date(),
+      largeImageKey: `logo_${game.platform.toLowerCase().replace(' ', '-')}`,
+      largeImageText: game.platform,
+      smallImageKey: imageKey,
+      smallImageText: imageText,
+      instance: false
+    };
+  } else {
+    return {
+      details: `Browsing The Archive`,
+      startTimestamp: new Date(),
+      largeImageKey: 'flashpoint',
+      largeImageText: 'BlueMaxima\'s Flashpoint',
+      instance: false
+    };
+  }
+}
+
+function formatOwners(owners: string) {
+  const split = owners.split(';');
+  if (split.length === 1) { 
+    return `By ${owners}`;
+  }
+  if (split.length === 2) {
+    return `By ${split[0].trim()} & ${split[1].trim()}`; 
+  }
+  return `By ${split[0].trim()} & ${split.length - 1} Others`;
+}
